6287c66ba8a0eafaf07dec53b955632706a03016
EgorNXD <e.nechaev@g.nsu.ru>
2023-04-16 20:04:48 +0700

    Version 2.2

--- Case #2.py
+++ Case #2.py
@@ -3,6 +3,8 @@ Salary: Polevik Alexey =
         Elizarev Yaroslav =
         Nechaev Egor =
 """
+import russian_localizator as ru
+
 
 dividend_bid = 0.0
 property_bid = 0.0
@@ -19,20 +21,19 @@ q2 = 0
 q3 = 0
 
 
-name = str(input('Здравствуйте! Как Вас зовут? '))
-print("Очень приятно,", name+"!")
-print("Сегодня мы посчитаем Ваш размер налога НДФЛ.")
-print("Пожалуйста, отвечайте на следующие вопросы честно и точно.")
-print("Начнем с общих вопросов.")
+name = str(input(ru.name))
+print(ru.glad, name+"!")
+print(ru.intention)
+print(ru.require)
+print(ru.start)
 
 
-ans = str(input('Вы резидент РФ? (более 183 дней в РФ - резидент) '))
-if ans.lower() == "да":
+ans = str(input(ru.rezident))
+if ans.lower() == ru.yes:
     res = True
 else: res = False
-ans = str(input('Ваша заработная плата, доходы от дивидендов,'
-                ' доходы от операций с ценными бумагами суммарно составляют более 5 млн рублей? '))
-if ans.lower() == "да":
+ans = str(input(ru.big_income))
+if ans.lower() == ru.yes:
     rich = True
 else: rich = False
 
@@ -41,13 +42,11 @@ if res == False:
     dividend_bid = 0.15
     property_bid = 0.3
     deal_bid = 0.3
-    ans = str(input('Относитесь ли вы к одной из следующих категорий работников: высококвалифицированный специалист,'
-                    ' беженец, участник Государственной программы по оказанию содействия добровольному переселению в Российскую Федерацию соотечественников,'
-                    ' проживающих за рубежом, а также членами их семей, член экипажа судна, плавающего под Российским флагом? '))
-    if ans.lower() == "да" and rich == True:
+    ans = str(input(ru.special_labor_category))
+    if ans.lower() == ru.yes and rich == True:
         wage_bid = 0.15
         wage_addition = 650000
-    elif ans.lower() == "да" and rich == False:
+    elif ans.lower() == ru.yes and rich == False:
         wage_bid = 0.13
     else: wage_bid = 0.3
 
@@ -61,40 +60,38 @@ else:
     else:
         wage_bid = 0.13
         deal_bid = 0.13
-        ans = str(input('Вы продавали имущество? '))
-        if ans.lower() == "да":
-            ans = str(input('Имущество было получено по наследству, '
-                            'в результате приватизации или в результате передачи по договору о пожизненном содержании с иждивением? '))
-            if ans.lower() == "да":
-                ans = str(input('Вы владели жильем более 3 лет? '))
-                if ans.lower() == "нет":
+        ans = str(input(ru.did_u_sold_pprty))
+        if ans.lower() == ru.yes:
+            ans = str(input(ru.property_ownrshp_condtns))
+            if ans.lower() == ru.yes:
+                ans = str(input(ru.own_three_yrs_or_not))
+                if ans.lower() == ru.no:
                     property_bid = 0.13
             else:
-                ans = str(input('Являлось ли Ваше жилье единственным располагаемым Вами или Вашим супругом на момент продажи? '))
-                if ans.lower() == "нет":
-                    ans = str(input('Вы владели жильем более 5 лет? '))
-                    if ans.lower() == "нет":
+                ans = str(input(ru.only_real_estate))
+                if ans.lower() == ru.no:
+                    ans = str(input(ru.own_five_yrs_or_not))
+                    if ans.lower() == ru.no:
                         property_bid = 0.13
-            ans = str(input('У Вас двое или более детей? '))
-            if ans.lower() == "да":
+            ans = str(input(ru.how_many_chldrn))
+            if ans.lower() == ru.yes:
                 property_bid = 0.0
         else: property_haver = False
 
 
-print("Хорошо, теперь перейдем к вычислению.")
-q1 = int(input('Введите Ваш годовой доход по облигациям, выпущенных до 1 января 2007 года: '))
-q2 = int(input('Введите Ваш годовой доход c выигрышей и призов, получаемых в проводимых конкурсах,'
-               ' играх и других мероприятиях в целях рекламы товаров, работ и услуг' ))
+print(ru.lets_get_down_counting)
+q1 = int(input(ru.old_bonds))
+q2 = int(input(ru.lotteries))
 if q2 <= 4000:
     q2 = 0
-q3 = int(input('Введите Ваши процентные доходы по вкладам в банках: '))
-dividend_income = int(input('Введите Ваш годовой доход по дивидентам: '))
+q3 = int(input(ru.bank_deposit_inc))
+dividend_income = int(input(ru.dividents ))
 if property_haver == True:
-    property_income = int(input('Введите Ваш годовой доход c продажи имущества: '))
-deal_income = int(input('Введите Ваш годовой доход c гражданско-правовых договоров (аренда имущества и тд): '))
-wage_income = int(input('Введите Ваш годовой доход c зарплаты: '))
+    property_income = int(input(ru.estate_sale_inc))
+deal_income = int(input(ru.citizens_deals_inc))
+wage_income = int(input(ru.salary))
 
 
-print("Отлично, вот сколько Вы должны: ")
+print(ru.total)
 print(round(dividend_income*dividend_bid + property_income*property_bid + deal_income*deal_bid + wage_income*wage_bid + wage_addition + q1*0.09 + q2*0.35 + q3*wage_bid))
 
